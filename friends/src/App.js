5c13719318383b6e47281658c9ef3533e3889789
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-17 11:00:04 -0800

    added Switch around Routes per RR docs

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-import Login from './components/Login';
-import FriendList from './components/FriendList';
-
+import Login from "./components/Login";
+import FriendList from "./components/FriendList";
 
 function App() {
   return (
-    <Router >
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Login</Link>
-            </li>
-            <li>
-              <Link to='/friends'>Friends List</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path='/login'></Route>
-        <Route path='/friends'></Route>
-      </div>
-      <Login />
-      <FriendList />
-    </Router >
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/friends">Friends List</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch> 
+            <Route path="/login" component={Login}>Login</Route>
+            <Route path="/friends" component={FriendList}>Friend List</Route>
+            <Route exact path="/">Home</Route>
+          </Switch>
+        </div>
+        <Login />
+        <FriendList />
+      </Router>
   );
 }
 
