9f3c0912104ac14f136354f8eec35542b7ca44e5
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-17 10:34:46 -0800

    set up/imported Router, Route, Link, Redirect

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const FriendList = () => {
+
+    return (
+        <div>
+            <h1>FriendList Component</h1>
+        </div>
+    )
+}
+export default FriendList;
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const Login = () => {
+
+    return (
+        <div>
+            <h1>Login Component</h1>
+        </div>
+    )
+}
+export default Login;
